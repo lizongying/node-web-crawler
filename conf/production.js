f1a4212b32739fb45ee8345780a87cf12b27edb1
Li Zongying <lizongying@msn.com>
2016-10-13 19:29:50 +0800

    update config

--- conf/production.js
+++ conf/production.js
@@ -7,10 +7,10 @@ var common = require('./common');
 function Production() {}
 
 Production.prototype= {
-    mysqlHost: 'rds33114q0a6t32npf55.mysql.rds.aliyuncs.com',//mysql地址
+    mysqlHost: '127.0.0.1',//mysql地址
     mysqlPort: 3306,//mysql端口
-    mysqlUser: 'shurufa',//mysql用户名
-    mysqlPassword: 'Shurufame302',//mysql密码
+    mysqlUser: 'root',//mysql用户名
+    mysqlPassword: '',//mysql密码
     mysqlDatabase: 'srf',//mysql数据库
     mongodbHost: '127.0.0.1',//mongodb地址
     mongodbPort: 27017,//mongodb端口
